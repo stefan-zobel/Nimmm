bb4e7e0c1ee007004bd21095ff8f35501332490a
stefan-zobel <stefan-zobel@users.noreply.github.com>
2022-10-09 23:10:31 +0200

    add non-quadratic Complex64 test

--- tests/test1.nim
+++ tests/test1.nim
@@ -5,10 +5,11 @@
 #
 # To run these tests, simply execute `nimble test`.
 
+import std/complex
 import unittest
 
 import Nimmm
-test "first for test []= and *":
+test "[2 x 2] * [2 x 2] float32 test for []= and *":
   var m1 = newMatrix[float32](2, 2)
   var m2 = newMatrix[float32](2, 2)
 
@@ -24,3 +25,19 @@ test "first for test []= and *":
 
   let c = m1 * m2
   echo c
+
+test "[2 x 2] * [2 x 1] Complex64 test for []= and *":
+  var m1 = newMatrix[Complex64](2, 2)
+  var m2 = newMatrix[Complex64](2, 1)
+
+  m1[0, 0] = complex64(55, -1)
+  m1[0, 1] = complex64(66, 1)
+  m1[1, 0] = complex64(77, 0)
+  m1[1, 1] = complex64(88, 2.5)
+
+  m2[0, 0] = complex64(99, 0.33)
+  m2[1, 0] = complex64(11, 0.7)
+
+  let c = m1 * m2
+  echo c
+
