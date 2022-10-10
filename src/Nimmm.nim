ed807bd77ddf8dc6d406baa292d347c0eeed19a4
stefan-zobel <stefan-zobel@users.noreply.github.com>
2022-10-11 00:26:10 +0200

    add `==` and `[]`

--- src/Nimmm.nim
+++ src/Nimmm.nim
@@ -1,5 +1,7 @@
-import nimmkl, nimmkl/[common, mulColMajor]
+import nimmkl/[common, mulColMajor]
 
+
+# ME = alias for matrix element type
 type
   ME* = SomeElementType
 
@@ -20,6 +22,9 @@ proc newMatrix[T: ME](rows, cols: int, a: seq[T]): Matrix[T] =
   result = Matrix[T](rows: rows, cols: cols)
   result.data = a
 
+proc `==`*[T: ME](a, b: Matrix[T]): bool =
+  a.rows == b.rows and a.cols == b.cols and a.data == b.data
+
 proc `$`*[T: ME](a: Matrix[T]): string =
   result = $(a.data)
 
@@ -29,5 +34,9 @@ proc `*`*[T: ME](a, b: Matrix[T]): Matrix[T]  =
   let k = a.cols
   result = newMatrix[T](m, n, mul[T](m, n, k, a.data, b.data))
 
-proc `[]=`*[T: ME](m: var Matrix[T], i, j: int, val: T) =
-  m.data[idx(i, j, m.rows)] = val
+proc `[]`*[T: ME](a: Matrix[T], i, j: int): T {. inline .} =
+  a.data[idx(i, j, a.rows)]
+
+proc `[]=`*[T: ME](a: var Matrix[T], i, j: int, val: T) {. inline .} =
+  a.data[idx(i, j, a.rows)] = val
+
